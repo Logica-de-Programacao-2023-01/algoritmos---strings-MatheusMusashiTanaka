c8215a2d5276d102e376fb4e4b750df3ba193a70
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-06-12 22:37:36 -0300

    Update algoritimo4.go

--- algoritimo4.go
+++ algoritimo4.go
@@ -1,1 +1,21 @@
+package main
 
+import "fmt"
+
+func main() {
+	var slice []int
+	var soma, tamanho, x int
+
+	fmt.Print("Digite o tamanho do Slice: ")
+	fmt.Scan(&tamanho)
+
+	for i := 0; i < tamanho; i++ {
+		fmt.Print("Digite um número: ")
+		fmt.Scan(&x)
+		slice = append(slice, x)
+		soma = soma + x
+	}
+
+	fmt.Println(slice)
+	fmt.Println(soma)
+}
