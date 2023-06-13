d4719cffa98cdb93bf5270ec8a4a462e8f299174
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-06-12 22:56:00 -0300

    Update algoritimo11.go

--- algoritimo11.go
+++ algoritimo11.go
@@ -1,1 +1,21 @@
 
+package main
+
+import "fmt"
+
+func main() {
+
+	array := [2][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+	}
+
+	var linha, coluna int
+
+	fmt.Print("Informe a linha: ")
+	fmt.Scanln(&linha)
+	fmt.Print("Informe a coluna: ")
+	fmt.Scanln(&coluna)
+
+	fmt.Printf("O valor armazenado na posição (%d,%d) é %d", linha, coluna, array[linha][coluna])
+}
