bda24c81a904ac18e559ca4bc1696d06462587de
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-06-12 22:39:00 -0300

    Update algoritimo7.go

--- algoritimo7.go
+++ algoritimo7.go
@@ -1,1 +1,29 @@
+package main
 
+import "fmt"
+
+func main() {
+
+	slice := []int{1, 2, 3, 4, 5}
+
+	var num int
+
+	fmt.Print("Informe um número inteiro: ")
+	fmt.Scanln(&num)
+
+	encontrado := false
+	for _, valor := range slice {
+
+		if num == valor {
+			encontrado = true
+			break
+		}
+	}
+
+	if encontrado {
+		fmt.Println("O número pertence à Slice!")
+	} else {
+		slice = append(slice, num)
+		fmt.Println("O número inserido foi adicionado à Slice:", slice)
+	}
+}
