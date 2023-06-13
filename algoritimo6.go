fbf2509885c28b70ee9e4f4668cfdc6064173866
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-06-12 22:38:43 -0300

    Update algoritimo6.go

--- algoritimo6.go
+++ algoritimo6.go
@@ -1,1 +1,27 @@
+package main
 
+import "fmt"
+
+func main() {
+	array := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	var num int
+
+	fmt.Print("Digite um número: ")
+	fmt.Scanln(&num)
+
+	encontrado := false
+	for _, valor := range array {
+
+		if num == valor {
+			encontrado = true
+			break
+		}
+	}
+	if encontrado {
+		fmt.Print("O seu número pertence a Array!")
+	} else {
+		fmt.Print("O seu número não pertence a Array!")
+	}
+
+}
