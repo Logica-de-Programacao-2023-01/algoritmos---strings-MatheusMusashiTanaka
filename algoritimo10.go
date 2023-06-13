f1c24819c29a24ff90763a46381cce1b5ee789a8
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-06-12 22:55:48 -0300

    Update algoritimo10.go

--- algoritimo10.go
+++ algoritimo10.go
@@ -1,1 +1,20 @@
+package main
 
+import "fmt"
+
+func main() {
+
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	min := 9999999999999
+	max := -999999999999
+
+	for _, x := range slice {
+		if x > max {
+			max = x
+		}
+		if x < min {
+			min = x
+		}
+	}
+	fmt.Println("O maior valor é", max)
+	fmt.Println("O menor valor é", min)
