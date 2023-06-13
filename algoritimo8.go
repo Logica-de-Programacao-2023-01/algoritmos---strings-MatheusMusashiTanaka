f59dbfe2aafd1e15ca357859800e0c65a716888d
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-06-12 22:39:24 -0300

    Update algoritimo8.go

--- algoritimo8.go
+++ algoritimo8.go
@@ -1,1 +1,21 @@
+package main
 
+import "fmt"
+
+func main() {
+
+	slice := []string{"a", "a", "b", "c", "d", "a", "ab", "c"}
+	var valor string
+	fmt.Print("Digite um valor: ")
+	fmt.Scan(&valor)
+
+	var resultado []string
+
+	for _, x := range slice {
+		if x != valor {
+			resultado = append(resultado, x)
+		}
+	}
+
+	fmt.Println(resultado)
+}
