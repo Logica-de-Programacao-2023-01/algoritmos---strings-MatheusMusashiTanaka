4d3a4d467cdbe03311b96078e083227b7cf145eb
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-06-12 22:39:40 -0300

    Update algoritimo9.go

--- algoritimo9.go
+++ algoritimo9.go
@@ -1,1 +1,17 @@
+package main
 
+import "fmt"
+
+func main() {
+
+	Array := [6]float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0}
+
+	var num float64
+	fmt.Print("Informe um número para repor todas as posições do Array: ")
+	fmt.Scanln(&num)
+
+	for i := 0; i < len(Array); i++ {
+		Array[i] = num
+	}
+	fmt.Println("Slice atual:", Array)
+}
