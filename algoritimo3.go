d5035df17c7ab7ce04594d9d155fe6303268cd74
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-06-12 22:37:17 -0300

    Update algoritimo3.go

--- algoritimo3.go
+++ algoritimo3.go
@@ -1,1 +1,13 @@
+package main
+import "fmt"
 
+func main() {
+  array := [4]float64{2.6,8.4,20.2,14.5}
+  var multiplicacao float64
+  
+  for _, x := range array {
+      multiplicacao = multiplicacao * x
+  }
+  produto := multiplicacao * float64(len(array))
+  fmt.Println("O produto de 2.6, 8.4, 20.2, 14.5 é", produto)
+}
