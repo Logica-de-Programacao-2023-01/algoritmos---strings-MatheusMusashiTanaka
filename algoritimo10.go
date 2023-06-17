eb75784d1a7f0ae9f11005f4199af55df434862b
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-06-16 21:48:36 -0300

    Update algoritimo10.go

--- algoritimo10.go
+++ algoritimo10.go
@@ -1,20 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// Escreva um programa que receba duas
+//strings e verifique se elas são anagramas.
+//Informe ao usuário se são ou não.
 
 func main() {
+	var x, y string
+	fmt.Print("Escreva algo: ")
+	fmt.Scan(&x)
+	fmt.Print("Escreva algo: ")
+	fmt.Scan(&y)
 
-	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	min := 9999999999999
-	max := -999999999999
+	if len(x) != len(y) {
+		fmt.Println("Não é um anagrama")
+	} else {
+		frequenciaX := make(map[rune]int)
+		frequenciaY := make(map[rune]int)
 
-	for _, x := range slice {
-		if x > max {
-			max = x
+		for _, letra := range x {
+			frequenciaX[letra] += 1
 		}
-		if x < min {
-			min = x
+
+		for _, letra := range y {
+			frequenciaY[letra] += 1
+		}
+
+		anagrama := true
+
+		for letra, count := range frequenciaX {
+			if frequenciaY[letra] != count {
+				anagrama = false
+			}
+		}
+
+		if anagrama {
+			fmt.Printf("As palavras %s e %s são anagramas\n", x, y)
+		} else {
+			fmt.Println("Não são anagramas")
 		}
 	}
-	fmt.Println("O maior valor é", max)
-	fmt.Println("O menor valor é", min)
+}
